baa32e1ed0108645cba34061587206020df1fbda
AnzhelDimitrov <108436278+AnzhelDimitrov@users.noreply.github.com>
2024-08-08 03:00:14 +0300

    Add basic CRUD operations and services for User, Product, and ShoppingCart

--- ecommerce/src/main/java/com/a/dimitrov/ecommerce/model/ShoppingCart.java
+++ ecommerce/src/main/java/com/a/dimitrov/ecommerce/model/ShoppingCart.java
@@ -30,6 +30,38 @@ public class ShoppingCart {
         this.quantity = quantity;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
     @Override
     public String toString() {
         return "ShoppingCart{" +
